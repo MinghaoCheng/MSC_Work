02d972b63d990df2cb98ab061028db9e6710970d
MinghaoCheng <37666532+MinghaoCheng@users.noreply.github.com>
2018-10-23 20:48:09 +0100

    Add files via upload

---
+++ Computer Architecture H/Assignment1/version1/TrafficLightRun.hs
@@ -0,0 +1,22 @@
+module Main where
+import HDL.Hydra.Core.Lib
+import TrafficLight
+
+main :: IO ()
+main = run_traffic testdata
+
+testdata :: [[Int]]
+testdata =
+    [[1], [0], [0], [0], [0], [0], [0], [0], [0], [0],
+     [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]]
+
+run_traffic :: [[Int]] -> IO ()
+run_traffic input = runAllInput input output
+    where
+        reset = getbit input 0
+        (green, amber, red) = trafficLight reset
+        output = [string " reset = ", bit reset,
+                  string " green = ", bit green,
+                  string " amber = ", bit amber,
+                  string " red = ", bit red]
+            
