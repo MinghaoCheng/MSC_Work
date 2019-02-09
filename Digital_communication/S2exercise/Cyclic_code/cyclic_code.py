04e27f4a09e27f8c2940016c5cf9a1073a80d7a5
Cheng <chengminghao92@gmail.com>
2019-02-09 17:58:44 +0000

    exercise updates

---
+++ Digital_communication/S2exercise/Cyclic_code/cyclic_code.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class cyclic_encoder:
+    def __init__(self):
+        self.Px = np.array([1,1,0,0,1])
+
+
+
+def main():
+    cyclic_Encoder = cyclic_encoder()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
